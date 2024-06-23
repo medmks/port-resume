a17a10094894bed53dd12cdf98d85e9abc2b3290
Mohamed <looklike882@gmail.com>
2024-06-23 20:43:31 +0100

    Init : Project

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,8 @@
 import type { MetaFunction } from "@remix-run/node";
+import Navbar from "~/components/navbar.component";
+import { Profile} from "~/components";
 
+Navbar
 export const meta: MetaFunction = () => {
   return [
     { title: "New Remix App" },
@@ -9,40 +12,8 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div className="font-sans p-4">
-      <h1 className="text-3xl">Welcome to Remix</h1>
-      <ul className="list-disc mt-4 pl-6 space-y-2">
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/quickstart"
-            rel="noreferrer"
-          >
-            5m Quick Start
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/tutorial"
-            rel="noreferrer"
-          >
-            30m Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/docs"
-            rel="noreferrer"
-          >
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+            <main className="bg-customColor h-screen w-full">
+                <Profile/>
+            </main>
   );
 }
---
+++ app/utilities/style.ts
@@ -0,0 +1,17 @@
+const styles = {
+          paddingX: "sm:px-16 px-6",
+          paddingY: "sm:py-16 py-6",
+          padding: "sm:px-40 px-6 sm:py-40 py-16",
+        
+          heroHeadText:
+            "font-outfit font-semibold text-white lg:text-[36px] sm:text-[60px] xs:text-[50px] text-[32px] lg:leading-[42px] ",
+          heroSubText:
+            "text-[#adc3df] font-outfit font-semibold lg:text-[32px] sm:text-[26px]  xs:text-[20px] text-[24px]   lg:leading-[42px] ",
+        
+          sectionHeadText:
+            "text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]",
+          sectionSubText:
+            "sm:text-[18px] font-outfitsubtext font-extralight  text-[18px]  text-white tracking-wider       ",
+        };
+        
+        export { styles };
