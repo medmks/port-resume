380059458e699c89a978bcb2b644a650faebb7c8
Mohamed <looklike882@gmail.com>
2024-06-29 12:58:05 +0100

    Add : work component

--- app/components/work.component.tsx
+++ app/components/work.component.tsx
@@ -2,45 +2,54 @@ import { motion } from "framer-motion";
 import { SectionWrapper } from "~/Hoc";
 import { fadeIn, textVariant } from "~/utilities/motion";
 import { styles } from "~/utilities/style";
-// import { MyWorks } from "~/constants"
-import { jobit } from "~/assets";
+import {  bbktour, randonne, multiservice } from "~/assets";
 import Divider from "./Divider.component";
 
+const worksexample = [
+  //  {image: jobit, source_code_link: "https://github.com" },
+  { image: bbktour, source_code_link: "https://github.com" },
+  { image: randonne, source_code_link: "https://github.com" },
+  { image: multiservice, source_code_link: "https://github.com" },
+];
 const ProjectCard = ({
   project,
   index,
-}: {
+}: {  
   project: { image: string; source_code_link: string };
   index: number;
 }) => {
   return (
-    <motion.div
-      variants={fadeIn("up", "spring", 0.5 * index, 0.75)}
-      key={index}
-    >
-      <div className="relative w-full">
-        <img
-          src={project.image}
-          alt="kk"
-          className="w-full h-full object-full  rounded-2xl "
-        />
-      </div>
-    </motion.div>
+    <div className="h-[250px] min-w-[300px] max-w-[350px]  ">
+      <motion.div
+        variants={fadeIn("up", "spring", 0.5 * index, 0.75)}
+        key={index}
+        className=" relative " 
+      >
+          <img
+            src={project.image}
+            alt={"project_image"}
+            className="  object-cover rounded-lg h-[250px] max-[335px] "
+          />
+      </motion.div>
+    </div>
   );
 };
 const Work = () => {
   return (
     <>
-      <motion.section variants={textVariant(0.5)}>
-        <h2 className={`${styles.sectionHeadText}`}>Work</h2>
-        <div className=" mt-10 flex flex-wrap gap-7">
-          {[{ image: jobit, source_code_link: "https://github.com" }].map(
-            (work, index) => {
-              return <ProjectCard key={index} project={work} index={index} />;
-            },
-          )}
-        </div>
-      </motion.section>
+      <section className="relative  w-full">
+        <motion.div variants={textVariant(0.5)}>
+          <h2 className={`${styles.sectionHeadText}`}>Work</h2>
+          <div className=" flex flex-col justify-center items-center mt-7 min-[900px]:grid grid-cols-2 lg:grid-cols-3  gap-4  ">
+                {worksexample.map(
+                  (work, index) => {
+                    return <ProjectCard key={index} project={work} index={index} />;
+                  },
+                )}
+              </div>
+        </motion.div>
+      </section>
+
       <Divider />
     </>
   );
