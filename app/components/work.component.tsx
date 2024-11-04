0376c436fc7a6c49b7ff96dce275107c9a6ba9a8
Mohamed <looklike882@gmail.com>
2024-11-04 23:27:40 +0100

    [Edit work component ]

--- app/components/work.component.tsx
+++ app/components/work.component.tsx
@@ -1,11 +1,14 @@
 import { motion } from "framer-motion";
 import { SectionWrapper } from "~/Hoc";
-import { fadeIn } from "~/utilities/motion";
 import { styles } from "~/utilities/style";
 import Divider from "./Divider.component";
-import { worksexample } from "~/constants";
+import { Variants, worksexample } from "~/constants";
+import { web } from "~/assets";
+import UserNavBar from "./userNavBar.component";
+import AnimationWrapper from "~/common/AnimationPresence";
+import { useState } from "react";
 
-const ProjectCard = ({
+export const ProjectCard = ({
   project,
   index,
 }: {
@@ -14,35 +17,60 @@ const ProjectCard = ({
     source_code_link: string;
     tags: { it: string }[];
     name: string;
-    url:string
+    url: string;
   };
   index: number;
 }) => {
   return (
     <>
-      <motion.div variants={fadeIn("up", "spring", 0.5 * index, 0.75)}>
-        <div
-          className=" bg-zinc-900 p-3 h-full  w-full xs:[350px] rounded-xl  shadow-md"
-        >
-          <div className="relative w-full ">
-            <img
-              src={project.image}
-              alt={project.image}
-              className="w-[330px] h-[250px]  object-contain  rounded-md "
-            />
-            {project.tags[0].it !== "WordPress" && (
-              <div className="flex justify-end absolute inset-0 m-3 card-img_hover">
-                <div className="  black-gradient h-10 w-10 rounded-full flex justify-center items-center cursor-pointer ">
-                  {/*  <img src={"github"} alt="" className="w-1/2 h-1/2 object-contain" />*/}
-                </div>
-              </div>
-            )}
+      {/* <motion.div variants={fadeIn("up", "spring", 0.5 * index, 0.75)}> */}
+      <div className=" bg-zinc-900 p-2 h-full  w-full xs:[350px] rounded-md  shadow-md">
+        <div className="relative w-full ">
+          <img
+            src={web}
+            alt={project.image}
+            className="w-[495px] object-contain rounded-md "
+          />
+          {project.tags[0].it !== "WordPress" && (
+            <div className="flex justify-end absolute inset-0 m-3 card-img_hover">
+              {/* <div className="  black-gradient h-10 w-10 rounded-full flex justify-center items-center cursor-pointer "> */}
+                {/*  <img src={"github"} alt="" className="w-1/2 h-1/2 object-contain" />*/}
+              {/* </div> */}
+            </div>
+          )}
+        </div>
+        <div className="flex flex-row justify-between items-center p-5 px-10">
+          <div className="">
+            <h2
+              className={`font-outfit text-white md:text-[30px] sm:text-[30px] xs:text-[40px] text-[28px]`}
+            >
+              {project.name}
+            </h2>
+            <p
+              className={`font-outfit text-white font-thin  md:text-[24px] sm:text-[22px] xs:text-[30px] text-[20px]`}
+            >
+              {project.tags[0].it}
+            </p>
           </div>
-          <div className="mt-2 flex flex-col p-2">
-            <a href={project.url} target="_blank"  rel="noreferrer" className={styles.roletext}>{project.name}</a>
-            {/* <p className=" leading-[25px] text-secondary text-[14px]" >{project.description}</p> */}
-          </div>
-          <div className="mt-2 flex flex-wrap gap-2 text-red-300 p-2  ">
+
+          <button className="border border-gray-500 rounded-full w-16 h-16 flex justify-center items-center   ">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.2}
+              stroke="white"
+              className="size-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25"
+              />
+            </svg>
+          </button>
+        </div>
+        {/* <div className="mt-2 flex flex-wrap gap-2 text-red-300 p-2  ">
             {project.tags.map((tag) => {
               return (
                 <div
@@ -55,33 +83,47 @@ const ProjectCard = ({
                 </div>
               );
             })}
-          </div>
-        </div>
-      </motion.div>
+          </div> */}
+      </div>
+      {/* </motion.div> */}
     </>
   );
 };
 
 const Work = () => {
+  const [filteredWork, setfilteredWork] = useState(worksexample);
+  const [active, setActive] = useState("Featured");
+
+  const SetFiltredWork = (Key: string) => {
+    if (!Key) {
+      console.error("Invalid key provided");
+      return;
+    }
+    setActive(Key);
+
+    const filtered = worksexample.filter((work) => work.type === Key);
+    setfilteredWork(filtered);
+    console.log(filtered);
+  };
+
   return (
     <>
-      <section className="relative  w-full">
-        <motion.div
-        // variants={textVariant(0.5)}
-        >
-          <div className="flex justify-between items-center">
-            <h2 className={`${styles.sectionHeadText} text-right`}>Work</h2>
-            <a href="/" target="_blank"  className={`${styles.sectionskillsText}  underline flex justify-end items-end text-left `} >See more</a>
-          </div>
-          
-          <div className=" flex flex-col justify-center items-center mt-7 min-[900px]:grid grid-cols-2 min-[1200px]:grid-cols-3  gap-4  ">
-            {worksexample.map((work, index) => {
-              return <ProjectCard key={index} project={work} index={index} />;
-            })}
-          </div>
-        </motion.div>
-      </section>
+      <AnimationWrapper>
+        <section className="relative w-full">
+          <motion.div>
+            <div className="flex justify-between items-center">
+              <h2 className={`${styles.sectionHeadText} text-right`}>Works</h2>
+            </div>
+            <UserNavBar
+              // active={active}
+              routes={Variants}
+              // SetFiltredWork={SetFiltredWork}
+            >
 
+            </UserNavBar>
+          </motion.div>
+        </section>
+      </AnimationWrapper>
       <Divider />
     </>
   );
