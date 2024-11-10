c8f8a02146aa5a51287d752c15c8cff6867a9aab
Mohamed <looklike882@gmail.com>
2024-11-10 22:10:03 +0100

    edit

--- app/assets/index.ts
+++ app/assets/index.ts
@@ -5,5 +5,29 @@ import bbktour from "./bbktour.png";
 import multiservice from "./multiservice.png";
 import profileImg from "./Web_Photo_Editor.jpg";
 import wec from "./wec.jpg";
+import AraBlog from "./localhost_5173_.png";
+import Image from "./image_1.jpg";
+import app from "./app2.jpg";
+import web from "./web.png";
+import Section_2 from "./Section_2.jpg";
+import WordPressIcon from "./icons/wordPress_icon.png";
+import shopify from "./icons/shopify_Icon.png";
 
-export { jobit, watermelon, bbktour, randonne, multiservice, profileImg, wec };
+export {
+  AraBlog,
+  jobit,
+  watermelon,
+  bbktour,
+  randonne,
+  multiservice,
+  profileImg,
+  wec,
+  WordPressIcon,
+  shopify,
+  folderStar,
+  // Travelweb,
+  Section_2,
+  app,
+  Image,
+  web,
+};
