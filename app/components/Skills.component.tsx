f39cae0be3507f132d6d943269ecfbf7a17e4b58
Mohamed <looklike882@gmail.com>
2024-11-10 22:10:21 +0100

    Tweak 1

--- app/components/Skills.component.tsx
+++ app/components/Skills.component.tsx
@@ -3,32 +3,39 @@ import { styles } from "~/utilities/style";
 import { skills } from "~/constants";
 import { SectionWrapper } from "~/Hoc";
 import Divider from "./Divider.component";
+import { staggerContainer } from "~/utilities/motion";
+import AnimationWrapper from "~/common/AnimationPresence";
 const Skill = () => {
   return (
     <>
       <motion.section
-        // variants={staggerContainer()}
+        variants={staggerContainer()}
         initial="hidden"
         whileInView="show"
         viewport={{ once: true, amount: 0.25 }}
-        // className={`z-max-w-7xl mx-auto relative z-0 `}
+        className={`relative w-fit `}
+
       >
-        <div className="relative w-fit mx-auto ">
+        <div className="">
           <h2 className={`${styles.sectionHeadText}`}>Skills</h2>
-          {/* Todo: add skills */}
-          <div className=" w-full relative">
-            <div className="max-w-fit flex flex-wrap gap-3 mt-5 min-w-fit relative align-middle overflow-visible  h-fit items-center place-content-center aspect-auto self-auto inset-auto">
-              {skills.map((skill, i) => (
+        </div>
+
+
+        <div className="max-w-fit flex flex-wrap gap-3 mt-5 min-w-fit relative align-middle overflow-visible  h-fit items-center place-content-center aspect-auto self-auto inset-auto">
+              {skills.map((skill, index) => (
+                          <AnimationWrapper
+                          key={index}
+                          animate={{ opacity: 1, scale: 1 }}
+                          transition={{ duration: 1, delay: index * 0.1 }}
+                        >
                 <div
                   className="  border border-gray-500 rounded-md p-3 w-fit"
-                  key={i}
                 >
                   <p className={`${styles.sectionskillsText}`}>{skill.title}</p>
                 </div>
+                </AnimationWrapper>
               ))}
             </div>
-          </div>
-        </div>
       </motion.section>
       <Divider />
     </>
