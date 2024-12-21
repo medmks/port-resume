06c7ee935212aad2aa87fecf9feca3dc283b5b94
Mohamed <looklike882@gmail.com>
2024-12-21 22:43:18 +0100

    [Finish] : service component

--- app/components/services.components.tsx
+++ app/components/services.components.tsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import { shopify } from "~/assets";
+import { ShopifyWhite } from "~/assets";
 import { staggerContainer } from "~/utilities/motion";
 import { styles } from "~/utilities/style";
 
@@ -10,51 +10,68 @@ const ServicesComponent = () => {
       variants={staggerContainer()}
     >
       <div className=" w-full flex flex-col bg-zinc-900  rounded-md">
+        {/* <h3 className={`text-center mt-5 font-outfit text-white md:text-[30px] sm:text-[30px] xs:text-[40px] text-[28px] flex gap-4 justify-center items-center mb-10`}> <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#ffffff" viewBox="0 0 256 256"><path d="M213.84,118.63a6,6,0,0,0-3.73-4.25L150.88,92.17l15-75a6,6,0,0,0-10.27-5.27l-112,120a6,6,0,0,0,2.28,9.71l59.23,22.21-15,75a6,6,0,0,0,3.14,6.52A6.07,6.07,0,0,0,96,246a6,6,0,0,0,4.39-1.91l112-120A6,6,0,0,0,213.84,118.63ZM106,220.46l11.85-59.28a6,6,0,0,0-3.77-6.8l-55.6-20.85,91.46-98L138.12,94.82a6,6,0,0,0,3.77,6.8l55.6,20.85Z"></path></svg> Services</h3> */}
         <div className="flex flex-col p-10 gap-10">
           <h2 className={`${styles.SubHeadText}`}>
             Our Mission Is To Make Your Business <br /> Better Through
             Technology{" "}
           </h2>
 
-          <div className="flex ">
+          <div className="flex gap-3 border border-white w-fit p-4 rounded-md bg-white items-center mt-5 mb-5">
+          <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#000000" viewBox="0 0 256 256"><path d="M213.85,125.46l-112,120a8,8,0,0,1-13.69-7l14.66-73.33L45.19,143.49a8,8,0,0,1-3-13l112-120a8,8,0,0,1,13.69,7L153.18,90.9l57.63,21.61a8,8,0,0,1,3,12.95Z"></path></svg>
+            <h3 className={`text-black font-normal font-outfit text-[22px]`}>Multiple Services </h3>
+          </div>
+
+          <div className="flex">
             <div className="w-1/3  h-72 flex flex-col justify-center gap-5 ">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                width="100"
-                height="100"
+                width="90"
+                height="90"
                 fill="#ffffff"
                 viewBox="0 0 256 256"
               >
-                <path d="M216,42H40A14,14,0,0,0,26,56V200a14,14,0,0,0,14,14H216a14,14,0,0,0,14-14V56A14,14,0,0,0,216,42Zm2,158a2,2,0,0,1-2,2H40a2,2,0,0,1-2-2V56a2,2,0,0,1,2-2H216a2,2,0,0,1,2,2ZM78,84A10,10,0,1,1,68,74,10,10,0,0,1,78,84Zm40,0a10,10,0,1,1-10-10A10,10,0,0,1,118,84Z"></path>
+                <path d="M180.7,172H107.81a44,44,0,1,1-79-30.41,4,4,0,0,1,6.4,4.81A35.67,35.67,0,0,0,28,168a36,36,0,0,0,72,0,4,4,0,0,1,4-4h76.7a12,12,0,1,1,0,8ZM64,180a12,12,0,0,0,9.33-19.54l37.11-60.3a4,4,0,0,0-1.31-5.51A36,36,0,1,1,161,49.58a4,4,0,1,0,7.33-3.19,44,44,0,1,0-66.71,52.83l-35.1,57.05A11.58,11.58,0,0,0,64,156a12,12,0,0,0,0,24Zm128-56a44,44,0,0,0-19.56,4.58l-35.11-57A12,12,0,1,0,128,76a12.24,12.24,0,0,0,2.52-.27L167.63,136a4,4,0,0,0,5.5,1.31A36,36,0,1,1,192,204a4,4,0,0,0,0,8,44,44,0,0,0,0-88Z"></path>
               </svg>
-              <h3 className={`${styles.sectionskillsText}`}>Web Development</h3>
+              <h3 className={`${styles.serviceHeadText} text-red-600`}>
+                Web Development
+              </h3>
               <p className={`${styles.sectionSubText}`}>
-                We build websites that are fast, secure, and easy to navigate.
+                We build websites that are fast, secure, and easy to navigate,
+                and that help you reach your goals.
               </p>
             </div>
             <div className="w-1/3  h-72 flex  flex-col justify-center gap-5 ">
-              <img className="w-24 h-24" src={shopify} alt="shopify-icon" />
-              <h3 className={`${styles.sectionskillsText}`}>
-                Mobile Development
+              <img
+                className="w-24 h-24"
+                src={ShopifyWhite}
+                alt="shopify-icon"
+              />
+              <h3 className={`${styles.serviceHeadText}`}>
+                Shopify apps Development
               </h3>
               <p className={`${styles.sectionSubText}`}>
-                We develop mobile applications that are user-friendly and
-                intuitive.
+                We create custom Shopify apps to help you scale your business or
+                help reach more customers.
               </p>
             </div>
             <div className="w-1/3  h-72 flex flex-col justify-center gap-5 ">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                width="100"
-                height="100"
+                width="90"
+                height="90"
                 fill="#ffffff"
                 viewBox="0 0 256 256"
               >
-                <path d="M236.24,19.76a6,6,0,0,0-8.48,0L173.94,73.57l-6.79-6.78a30,30,0,0,0-42.42,0L100,91.51l-7.76-7.75a6,6,0,0,0-8.48,8.48L91.51,100,66.79,124.73a30,30,0,0,0,0,42.42l6.78,6.79L19.76,227.76a6,6,0,1,0,8.48,8.48l53.82-53.81,6.79,6.78a30,30,0,0,0,42.42,0L156,164.49l7.76,7.75a6,6,0,0,0,8.48-8.48L164.49,156l24.72-24.73a30,30,0,0,0,0-42.42l-6.78-6.79,53.81-53.82A6,6,0,0,0,236.24,19.76Zm-113.45,161a18,18,0,0,1-25.46,0L75.27,158.67a18,18,0,0,1,0-25.46L100,108.49,147.51,156Zm57.94-57.94L156,147.51,108.49,100l24.72-24.73a18,18,0,0,1,25.46,0l22.06,22.06a18,18,0,0,1,0,25.46ZM90.43,34.23a6,6,0,0,1,11.14-4.46l8,20a6,6,0,1,1-11.14,4.46Zm-64,59.54a6,6,0,0,1,7.8-3.34l20,8a6,6,0,1,1-4.46,11.14l-20-8A6,6,0,0,1,26.43,93.77Zm203.14,68.46a6,6,0,0,1-7.8,3.34l-20-8a6,6,0,0,1,4.46-11.14l20,8A6,6,0,0,1,229.57,162.23Zm-64,59.54a6,6,0,1,1-11.14,4.46l-8-20a6,6,0,0,1,11.14-4.46Z"></path>
+                <path d="M208,36H48A12,12,0,0,0,36,48V208a12,12,0,0,0,12,12H208a12,12,0,0,0,12-12V48A12,12,0,0,0,208,36ZM88,156a12,12,0,1,1-12,12A12,12,0,0,1,88,156ZM44,208V48a4,4,0,0,1,4-4H84V148.4a20,20,0,1,0,8,0V105.66l56,56V212H48A4,4,0,0,1,44,208Zm168,0a4,4,0,0,1-4,4H156V160a4,4,0,0,0-1.17-2.83L92,94.34V44h40V72a4,4,0,0,0,1.17,2.83L151.33,93A20,20,0,1,0,157,87.33l-17-17V44h68a4,4,0,0,1,4,4ZM168,92a12,12,0,1,1-12,12A12,12,0,0,1,168,92Z"></path>
               </svg>
-              <h3 className={`${styles.sectionskillsText}`}>SEO</h3>
+              <h3 className={`${styles.serviceHeadText}`}>
+                {" "}
+                System and apps optimization
+              </h3>
               <p className={`${styles.sectionSubText}`}>
-                We optimize your website to rank higher on search engines.
+                We optimize your systems and apps to make them faster, more
+                efficient, and more secure.
               </p>
             </div>
           </div>
